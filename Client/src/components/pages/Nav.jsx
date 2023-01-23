968b697a7de9497c9677f85d5718d747c1d445ed
suhyun-yoo <wau3384@gmail.com>
2023-01-23 22:25:05 +0900

    로그인 팝업창 구현

--- Client/src/components/pages/Nav.jsx
+++ Client/src/components/pages/Nav.jsx
@@ -12,6 +12,7 @@ function Nav() {
         $('.wrap .submenu').toggleClass('active');
     };
 
+    // submenu > menu 버튼 클릭 시, menu slide 영역으로 화면 이동하기 
     const click_menuSub = () => {
         $('.wrap header .hamburger').removeClass('active');
         $('.wrap .submenu').removeClass('active');
@@ -19,8 +20,19 @@ function Nav() {
             scrollTop : $('.sec1').offset().top
         },1000)
     }
+
     // icon-box 영역 클릭 시, 로그인 및 회원가입 팝업창 띄우기
-    
+    const iconBox = () => {
+        $('.wrap .loginBox').addClass('active');
+    };
+    // closeBtn 클릭 시, 로그인 및 회원가입 팝업창 닫기
+    // $('.wrap .loginBox.active .login-txt .closeBtn').on('click',function(){
+    //     $('.wrap .loginBox').removeClass('active');
+    // });
+
+    const closeModal = () => {
+        $('.wrap .loginBox').removeClass('active');
+    }
 
     return (
         <div className="wrap">
@@ -30,7 +42,7 @@ function Nav() {
                         <span></span><span></span><span></span>
                     </div>
                     <div className="logo"> PUIS </div>
-                    <div className="icon-box">
+                    <div className="icon-box" onClick={iconBox}>
                         <svg viewBox="0 0 80 80" xmlns="http://www.w3.org/2000/svg">
                         <path d="M60 70.0002H20C17.7909 70.0002 16 68.2094 16 66.0002V64.6639C16 58.8513 19.6061 53.6486 25.049 51.6087C34.6889 47.9957 45.3111 47.9957 54.951 51.6087C60.3939 53.6486 64 58.8513 64 64.6639V66.0002C64 68.2094 62.2091 70.0002 60 70.0002Z" stroke="#F1F2F0" strokeLinecap="round" strokeLinejoin="round" />
                         <path d="M33.901 38.8673C37.729 40.8336 42.2702 40.8336 46.0981 38.8673C49.6607 37.0373 52.2131 33.7042 53.0512 29.7878L53.2748 28.7425C54.1318 24.7375 53.2164 20.5576 50.764 17.2773L50.4049 16.797C47.9521 13.5163 44.0958 11.5845 39.9996 11.5845C35.9034 11.5845 32.047 13.5163 29.5943 16.797L29.2352 17.2773C26.7827 20.5576 25.8674 24.7375 26.7243 28.7425L26.948 29.7878C27.786 33.7042 30.3385 37.0373 33.901 38.8673Z" stroke="#F1F2F0" strokeLinecap="round" strokeLinejoin="round" />
@@ -46,8 +58,21 @@ function Nav() {
             </ul>
 
             <div className="loginBox">
-                <input type="text" placeholder='ID'/>
+                <div className="login-txt">
+                    <div className="closeBtn" onClick={closeModal}>
+                        <span></span><span></span>
+                    </div>
+                    <h2>Login</h2>
+                    <div className="input-box">
+                        <input type="text" placeholder='ID'/>
+                        <input type="password" placeholder='Password' />
+                        <a className="submitBtn">로그인</a>
+                    </div>
+                    
+                    <a className='alarm'>회원가입하기</a>
+                </div>
             </div>
+            
         </div>
     );
 }
