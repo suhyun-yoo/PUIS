7afa77916b0566426418a7c4e7006cfa8f2cc9d7
suhyun-yoo <wau3384@gmail.com>
2023-02-01 12:39:29 +0900

    fooer 영역 구조 및 스타일링 진행

--- Client/src/components/pages/Footer.jsx
+++ Client/src/components/pages/Footer.jsx
@@ -4,15 +4,36 @@ import "../css/reset.css";
 import "../css/style.css";
 // jquey 연결
 import $ from "jquery";
+// img 연결
+import instagram from '../img/instagram.png';
+import kakaoTalk from '../img/kakao-talk.png';
+
 
 function Footer() {
   return (
     <footer>
         <div className="footer-left">
-            <div className="footer-left-top"></div>
-            <div className="footer-left-body"></div>
+            <div className="footer-left-top"><span>PUIS</span> DESSERT</div>
+            <div className="footer-left-middle">TEL : 010-4111-2238</div>
+            <div className="footer-left-bottom">
+              <a className="footer-icon" href="https://www.instagram.com/__puis/" target="_blank">
+                <img src={instagram} alt="instagram icon" />
+              </a>
+              <a className="footer-icon" href="https://smartstore.naver.com/__puis" target="_blank">
+                <p>N</p>
+              </a>
+              <a className="footer-icon" href="https://open.kakao.com/" target="_blank">
+                <img src={kakaoTalk} alt="kakao-talk icon"/>
+              </a>
+            </div>
         </div>
-        <div className="footer-right"></div>
+        <div className="footer-right">
+          <p>상호명 : 퓨이 (Puis)</p>
+          <p>대표자 : 선승완</p>
+          <p>사업자등록번호 : 3880302461</p>
+          <p>사업장 소재지 : 경기도 고양시 일산로316번길 53-6</p>
+          <p>E-mail : topsum416@naver.com</p>
+        </div>
     </footer>
   );
 }
