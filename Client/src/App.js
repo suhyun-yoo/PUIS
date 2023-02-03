113cb28eb110682843f130b07384f40dd7498deb
suhyun-yoo <wau3384@gmail.com>
2023-02-03 22:55:54 +0900

    윈도우 화면 이동 시, HTML 문서 최상단으로 이동하도록 scrollTop 추가하기

--- Client/src/App.js
+++ Client/src/App.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 
+// Link 이동 시, 페이지 최상단으로 이동하며 새로고침 되도록 하기
+import ScrollToTop from './components/pages/scrollTop';
+
 // 페이지 import
 import Nav from './components/pages/Nav';
 import Main from './components/pages/Main';
 import Footer from './components/pages/Footer';
 import SideBar from './components/pages/Side-bar';
+import MenuDetail from './components/pages/MenuDetail';
 
 function App() {
   return (
     <BrowserRouter>
       <Nav/>
+      <ScrollToTop/>
       <Routes>
         <Route path='/' element={<Main/>} />
+        <Route path='/menu' element={<MenuDetail/>} />
       </Routes>
       <SideBar/>
       <Footer/>
