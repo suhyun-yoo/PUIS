ee94754107f4faf8f065f25789d5087f8b084e06
suhyun-yoo <wau3384@gmail.com>
2023-02-03 23:12:30 +0900

    필요없는 부분 삭제

--- Client/src/components/pages/MenuDetail.jsx
+++ Client/src/components/pages/MenuDetail.jsx
@@ -8,12 +8,6 @@ import "../css/style.css";
 import $ from "jquery";
 
 function MenuDetail() {
-  $(window).on('scroll', function(){
-    $('.slide-page-btns .page-btn:nth-child(1)').addClass('active');
-    $('.slide-page-btns .page-btn:nth-child(2)').removeClass('active');
-    $('.slide-page-btns .page-btn:nth-child(3)').removeClass('active');
-    $('.slide-page-btns .page-btn:nth-child(4)').removeClass('active');
-  });
     return (
     <div className="menuDetail">
       <h1>MENU DETAIL</h1>
