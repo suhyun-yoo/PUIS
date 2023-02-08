f0a6c0cebfd23b5ae3d6a281f7a2a99ca62d23cb
suhyun-yoo <wau3384@gmail.com>
2023-02-08 17:13:45 +0900

    MenuDetail 페이지 module.css 연결

--- Client/src/components/pages/MenuDetail.jsx
+++ Client/src/components/pages/MenuDetail.jsx
@@ -4,39 +4,38 @@ import React from "react";
 import pudding from '../img/다크초코푸딩.jpg'
 // css 연결
 import "../css/reset.css";
-import "../css/style.css";
-// jquey 연결
-import $ from "jquery";
+// import "../css/style.css";
+import styles from '../css/MenuDetail.module.css';
 
 function MenuDetail() {
     return (
-    <div className="menuDetail">
-      <div className="menuDetail-head">
-        <div className="menuDetail-head-left">
-          <div className="tag">Pudding</div>
-          <div className="img-box">
+    <div className={styles.menuDetail}>
+      <div className={styles.menuDetail_head}>
+        <div className={styles.menuDetail_head_left}>
+          <div className={styles.tag}>Pudding</div>
+          <div className={styles.imgBox}>
             <img src={pudding} alt="다크초코푸딩" />
           </div>
         </div>
-        <div className="menuDetail-head-right">
-          <p className="menuName">다크초코푸딩</p>
-          <p className="price">Price : <span>4.0</span></p>
-          <div className="order-wrap">
+        <div className={styles.menuDetail_head_right}>
+          <p className={styles.menuName}>다크초코푸딩</p>
+          <p className={styles.price}>Price : <span>4.0</span></p>
+          <div className={styles.order_wrap}>
             <input type="number" step={1} placeholder='수량을 선택하세요'/>
-            <div className="btn-wrap">
-              <button type="submit" className="cartBtn">장바구니 추가</button>
+            <div className={styles.btn_wrap}>
+              <button type="submit" className={styles.cartBtn}>장바구니 추가</button>
               <button type="submit">주문하러 가기</button>
             </div>
           </div>
-          <p className="cnt">재고수량 : <span>15</span>개</p>
+          <p className={styles.cnt}>재고수량 : <span>15</span>개</p>
         </div>
       </div>
       <hr />
-      <div className="menuDetail-body">
-        <div className="img-box"></div>
+      <div className={styles.menuDetail_body}>
+        <div className={styles.imgBox}></div>
       </div>
-      <div className="menuDetail-bottom">
-        <div className="review-box"></div>
+      <div className={styles.menuDetail_bottom}>
+        <div className={styles.reviewBox}></div>
       </div>
     </div>
   );
